f971606ab5c5b36f8f3b319c57288288a1e75ab5
Jorge Siles <jorges@sintesis.com.bo>
2017-06-07 11:43:02 -0400

    requerimientos

--- admin/code/template/subastasViewTpl.php
+++ admin/code/template/subastasViewTpl.php
@@ -248,6 +248,13 @@ $prod = $db->next_record();
 				<td><?=$prod["sub_tiempo"]?>
 				</td>
 			</tr>	
+                        <tr>
+				<td>Notificaci&oacute;n N horas antes:</td>
+				<td>
+				
+				</td>
+			</tr>
+
 		<tr><td colspan="2">
          <?php $uidTpl=$prod["sub_uid"];
                                   include("./code/execute/listadoOfertas.php");
--- admin/core/sendMail.php
+++ admin/core/sendMail.php
@@ -2,7 +2,7 @@
   require_once "../phpmailer/class.phpmailer.php";
   require_once "./mail.cfg";
   require_once "./admin.php";
-  set_time_limit(310);
+  set_time_limit(600);
   
   $sSQL="select * from mdl_notificacion_envio where noe_status=0 and noe_retry<=2 and noe_email!=''";
   $nroReg=$db->numrows($sSQL);
@@ -38,9 +38,11 @@
           }
 	  $mail->Subject =admin::getDbValue("select not_subject from mdl_notificacion_template where not_tip_uid=".$noe["noe_nti_uid"]);
 	  $mail->Body =admin::getDbValue("select concat(not_template,'\n',not_sign) as body from mdl_notificacion_template where not_tip_uid=".$noe["noe_nti_uid"]);
-     /* if($noe["noe_attach"]!=""){
-        $mail->addAttachment(PATH_ROOT.$noe["noe_attach"]);
-      }*/
+            if($noe["noe_nti_uid"]==2){    
+              if($noe["noe_attach"]!=""){
+                $mail->addAttachment(PATH_ROOT.$noe["noe_attach"]);
+              }
+            }
 	  $mail->CharSet = 'UTF-8';
         $exito = $mail->Send();
         $intentos=1; 
@@ -51,13 +53,46 @@
       }
       if(!$exito)
       {
-             admin::getDbValue("update mdl_notificacion_envio set noe_retry=$intentos, noe_response='".$mail->ErrorInfo."' where noe_uid=".$noe["noe_uid"]);
+             admin::getDbValue("update mdl_notificacion_envio set noe_retry=$intentos, noe_response='".$mail->ErrorInfo."', noe_fecha_envio=GETDATE() where noe_uid=".$noe["noe_uid"]);
       }
       else
       {
-             admin::getDbValue("update mdl_notificacion_envio set noe_retry=$intentos, noe_status=1, noe_response='OK' where noe_uid=".$noe["noe_uid"]);	
+             admin::getDbValue("update mdl_notificacion_envio set noe_retry=$intentos, noe_status=1, noe_response='OK', noe_fecha_envio=GETDATE() where noe_uid=".$noe["noe_uid"]);	
       } 
     }
   }
   echo "Fin de envio";
+  echo "Inicio avisos";
+  
+   $sSQL="select * from mdl_notificacion_previa where noe_estado=0 and noe_datetime<GETDATE()";
+   $nroReg=$db->numrows($sSQL);
+   if($nroReg>0){
+    $db->query($sSQL);
+    while($nop=$db->next_record())
+    {
+        $sSQL="select inc_cli_uid from mdl_incoterm where inc_sub_uid=".$nop["nop_sub_uid"];
+        $nroReg=$db->numrows($sSQL);
+        //echo $nroReg;
+        if($nroReg>0){
+            $db->query($sSQL);
+            while($list=$db->next_record())
+            {
+                $cli_uid=$list["inc_cli_uid"];
+                $cli_email=admin::getDbValue("select cli_mainemail from mdl_client where cli_uid=$cli_uid");
+                $nti_uid=1;
+                $attach =admin::getDbValue("select pro_document from mdl_product where pro_sub_uid=$sub_uid");
+                if(strlen($attach)>0) $attach="/docs/subasta/".$attach;
+                else $attach="";
+                admin::insertMail($cli_uid, $nti_uid, $attach, $cli_email, $sub_uid);
+                $cli_email=admin::getDbValue("select cli_commercialemail from mdl_client where cli_uid=$cli_uid");
+                if(strlen($cli_email)>0){
+                    admin::insertMail($cli_uid, $nti_uid, $attach, $cli_email, $sub_uid);
+                }
+                 admin::getDbValue("update mdl_notificacion_previa set noe_estado=1 where nop_uid=".$nop["nop_uid"]);
+            }
+        }
+
+    }
+   }
+  echo "Fin avisos";
 ?>
