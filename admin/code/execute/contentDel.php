09b88b6388a39be5f25dbae110e7109b578ba8d5
Jorge Siles <jorges@sintesis.com.bo>
2017-06-20 18:26:32 -0400

    correcciones

--- admin/code/execute/contentDel.php
+++
@@ -1,9 +0,0 @@
-<?php
-// echo "en contruccion"; die; 
-include_once("../../core/admin.php");
-admin::initialize('content','contentList',false); 
-//con_uid
-// contamos todos los registros que se encuentran en un nivel del contenido
- $sql = "update mdl_contents set con_delete=1 where con_uid='" . admin::toSql(admin::getParam("con_uid"),"Text")."'";
-$db->query($sql);
-?>
--- admin/code/execute/multipleDocsDel.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../../core/admin.php");
-admin::initialize('content','contentDocDel'); 
-
-$cdo_uid=admin::getParam("uid");
-
- $sql = "update tbl_publicaciones_docs set cdo_delete=1 where cdo_uid=".$cdo_uid;
-$db->query($sql);
-?>
